87ed4462c0bd4e460fdbf02708c60eec07671883
Alex <alecxandroch@gmail.com>
2024-12-17 12:28:43 -0300

    Setup Netlify deployment configuration9

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -4,18 +4,7 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter(),
-		prerender: {
-			handleHttpError: ({ path, referrer, message }) => {
-				// ignore deliberate link to shiny 404 page
-				if (path === '/not-found' && referrer === '/blog/how-we-built-our-404-page') {
-					return;
-				}
-
-				// otherwise fail the build
-				throw new Error(message);
-			}
-		}
+		adapter: adapter()
 	},
 	preprocess: vitePreprocess()
 };
