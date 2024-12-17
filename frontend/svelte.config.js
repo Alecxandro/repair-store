1d6c1f32d5f82af4724818438b32aa56d805dda3
Alex <alecxandroch@gmail.com>
2024-12-17 11:50:04 -0300

    Setup Netlify deployment configuration

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,15 +1,17 @@
 import adapter from '@sveltejs/adapter-netlify';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import { preprocessMeltUI } from '@melt-ui/pp';
+import sequence from 'svelte-sequential-preprocessor';
+import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter(),
-		alias: {
-			$lib: 'src/lib'
-		}
+		adapter: adapter()
 	},
-	preprocess: vitePreprocess()
+	preprocess: sequence([
+		preprocess(),
+		preprocessMeltUI()
+	])
 };
 
 export default config;
