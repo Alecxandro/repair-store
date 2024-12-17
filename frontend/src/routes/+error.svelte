5fa2d6bbcf630aa806dc46405a53f6d464ce97f7
Alex <alecxandroch@gmail.com>
2024-12-17 12:22:22 -0300

    Setup Netlify deployment configuration8

---
+++ frontend/src/routes/+error.svelte
@@ -0,0 +1,16 @@
+<script>
+    import { page } from '$app/stores';
+</script>
+
+<div class="error-container">
+    <h1>{$page.status}: {$page.error?.message}</h1>
+</div>
+
+<style>
+    .error-container {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 100vh;
+    }
+</style> 
--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -4,12 +4,17 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter({
-			edge: false,
-			split: true
-		}),
-		paths: {
-			base: ''
+		adapter: adapter(),
+		prerender: {
+			handleHttpError: ({ path, referrer, message }) => {
+				// ignore deliberate link to shiny 404 page
+				if (path === '/not-found' && referrer === '/blog/how-we-built-our-404-page') {
+					return;
+				}
+
+				// otherwise fail the build
+				throw new Error(message);
+			}
 		}
 	},
 	preprocess: vitePreprocess()
